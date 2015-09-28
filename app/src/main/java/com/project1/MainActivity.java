28dc57075f1896402f3730923e876fdc2e76a3fd
BLUEHORSE 123 <siddhartha.ee.13@gmail.com>
2015-09-28 11:25:53 +0530

    hh

--- app/src/main/java/com/project1/MainActivity.java
+++ app/src/main/java/com/project1/MainActivity.java
@@ -23,20 +23,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void textViewAdd() {
-        EditText et1 = new EditText(this);
-        EditText et2 = new EditText(this);
-        EditText et3 = new EditText(this);
-        EditText et4 = new EditText(this);
-        et1.setHint("Dynamic EditText!");
-        et2.setHint("Dynamic EditText!");
-        et3.setHint("Dynamic EditText!");
-        et4.setHint("Dynamic EditText!");
-        et1.setMinLines(1);
-        et1.setMaxLines(3);
-        flCont.addView(et1);
-        flCont.addView(et2);
-        flCont.addView(et3);
-        flCont.addView(et4);
+
+        for(int i = 0;i<10;i++)
+        {
+            EditText et = new EditText(this);
+            et.setHint("Dynamic EditText!");
+            et.setTag(i);
+            flCont.addView(et);
+        }
+
     }
 
 
